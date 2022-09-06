09a9662ef647064abd1fa564b76697672c83b41b
Kacper Zacharski <puffercatt@gmail.com>
2022-09-06 10:34:32 +0200

    1.0.2

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "xyz.puffercat.blurrywalls.android"
-    const val version = 2
-    const val versionName = "1.0.1"
+    const val version = 3
+    const val versionName = "1.0.2"
 }
